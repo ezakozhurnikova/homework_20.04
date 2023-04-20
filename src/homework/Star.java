a41ecc9ff5a8419bb8c183a4f3a49311970ad4d1
ezakozhurnikova <ekaterina.o.zak@gmail.com>
2023-04-20 17:26:14 +0200

    first commit

---
+++ src/homework/Star.java
@@ -0,0 +1,21 @@
+package homework;
+
+public class Star extends Thread {
+    Star(String name){
+        super(name);
+    }
+    public void run() {
+        System.out.println(getName()+" starts");
+        try {
+
+            for (int counter = 0; counter < 100; counter++) {
+                System.out.print("*");
+                Thread.sleep(100);
+               // System.out.println("in thread " + getName()+ " counter is " + counter);
+            }
+        }catch (InterruptedException e){
+            System.out.println(getName()+" interrupted");
+        }
+        System.out.println("end of "+getName());
+    }
+}
